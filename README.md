16deb20224c52fede82bc05d2fd16c7b99db7e55
Joe Lipson <joe.lipson@abc.net.au>
2020-11-23 22:22:45 +1100

    add install script

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# pi_wireless_bridge
+# Raspberry Pi wireless bridge
+
+Use a Rasperry Pi as a WiFi bridge! I wired up my house so rooms that are a long way from the WiFi router can still get decent internet. Problem is some devices like Google Chromecast and the Nintendo Switch are wireless only. By connecting a Raspberry Pi to wired ethernet then using it as a WiFi bridge these devices get a strong signal.
+
+All this does is automate the steps from the excelent guide at https://www.raspberrypi.org/documentation/configuration/wireless/access-point-bridged.md
+
+## Installation
+
+ssh to the pi you want to use as the bridge
+
+download the code 
+```
+curl -L -O https://github.com/shabadoo75/pi_wireless_bridge/archive/main.zip
+```
+
+unzip
+```
+unzip main.zip
+```
+
+install
+```
+cd pi_wireless_bridge-main
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Welcome to the Raspberry Pi wireless bridge setup!"
+echo
+echo
+
+echo "Installing Ansible"
+sudo apt-get update
+sudo apt-get -y install ansible
+echo
+echo
+
+read -e -p "Hostname for the Pi: " -i "bridge" hostname
+
+read -e -p "WiFi country code: " -i "AU" counry_code
+
+read -e -p "ssid for your bridge: " -i "home_bridge" ssid_name
+
+read -e -sp "WiFi password for your bridge: " wpa_passphrase
+echo
+
+read -e -p "WiFi mode [a,b,g]: " -i "g" wifi_mode
+
+read -e -p "WiFi channel: " -i "11" wifi_channel
+
+echo
+
+echo "****************************"
+echo "Configuration values:"
+echo "Hostname: $hostname"
+echo "Country code: $counry_code"
+echo "Bridge ssid: $ssid_name"
+echo "Bridge WiFi password: ********"
+echo "WiFi mode: $wifi_mode"
+echo "WiFi channel: $wifi_channel"
+echo "****************************"
+echo
+
+read -e -p "Looks good, start the install? [Y/N]: " -i "N" install
+if [ "$install" = "Y" ]; then
+  echo "starting..."
+else
+  echo "exit"
+  exit 0
+fi
+
+ansible-playbook -u pi \
+    -e "hostname=$hostname" \
+    -e "wpa_passphrase=$wpa_passphrase" \
+    -e "counry_code=$country_code" \
+    -e "ssid_name=$ssid_name" \
+    -e "wifi_mode=$wifi_mode" \
+    -e "wifi_channel=$wifi_channel" \
+    ansible/playbook.yml
